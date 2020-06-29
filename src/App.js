4e13782256aa1364a0527840fc82834d42404afb
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-06-29 15:36:21 +0200

    Transform App to class component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>
           Demo <code>ReactJS</code> App.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </h2>
+      </div>
+    );
+  }
 }
 
 export default App;
