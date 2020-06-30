6f2cd7adae8bbc9e16c346668a5fff104a3e2fc8
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-06-30 11:07:10 +0200

    Play with JSX

---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hello = () => {
+
+  return (
+    <div id='hello' className='dummyClass'>
+      <h2>Play with JSX </h2>
+    </div>
+  )
+
+}
+
+export default Hello;
