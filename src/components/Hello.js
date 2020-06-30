195dc8788dbcb8c1971ccc660c53ae847c71309d
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-06-30 11:07:34 +0200

    Play without JSX

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 const Hello = () => {
-
+  /*
   return (
     <div id='hello' className='dummyClass'>
       <h2>Play with JSX </h2>
     </div>
   )
+  */
+  return React.createElement(
+    'div', 
+    {id:'hello', className:'dummyClass'}, 
+    React.createElement('h2', null, 'Play without JSX')
+  );
 
 }
 
