a73015dfa7224fcbf3c7a9bf780e7fa6f6ee8d1d
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-07-01 09:16:36 +0200

    Thanks to state we gain ability to change message

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
 class Message extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      message: 'Welcome amigo'
+    }
+  }
+
   render() {
     console.log(this.props);
-    return <h3>Welcome friend!</h3>
+  return <h3>{this.state.message}!</h3>
   }
 }
 
