aa27641b9236e7d133adc8593507b25484fd27cb
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-07-01 09:36:51 +0200

    Never modify the state directly

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Message/>
+        <Counter/>
+        {/* <Message/> */}
         {/*
         <Greet name='Al' heroName='Batman'>
           <p>This is a child props/component</p>
