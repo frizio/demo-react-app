fcedecf28f375a615157fc54ecf241c9fec79a82
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-07-08 18:17:23 +0200

    Styling 3

--- src/components/Stylesheet.js
+++ src/components/Stylesheet.js
@@ -7,7 +7,7 @@ function Stylesheet(props) {
 
   return (
     <div>
-      <h2 className={className}>Demo stylesheet</h2>
+      <h2 className={`${className} font-xl`}>Demo stylesheet</h2>
     </div>
   )
 }
