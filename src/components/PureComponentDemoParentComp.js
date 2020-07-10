13f87ad6ae46f43df266b60007cb0520a971c69c
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-07-10 11:53:07 +0200

    Shallow comparison on props and state of component

--- src/components/PureComponentDemoParentComp.js
+++ src/components/PureComponentDemoParentComp.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import PureCompoentDemoRegularComp from './PureComponentDemoRegularComp';
 import PureComponentDemo from './PureComponentDemo';
 
-class PureComponentDemoParentComp extends Component {
+class PureComponentDemoParentComp extends PureComponent {
 
   constructor(props) {
     super(props)
